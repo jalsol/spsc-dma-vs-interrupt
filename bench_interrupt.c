212d523113163017a1174444c2c5c96898fae500
jalsol <nqtruong.1002@gmail.com>
2026-04-05 20:55:01 +0700

    Fix

--- bench_interrupt.c
+++ bench_interrupt.c
@@ -8,19 +8,11 @@
 #include "bench_common.h"
 
 #define DEVICE_PATH "/dev/dev_interrupt"
-#define BATCH_SIZE 1024
 
 int main(void) {
   int fd = open(DEVICE_PATH, O_RDONLY);
   if (fd < 0) {
     perror("open");
-    return 1;
-  }
-
-  uint32_t *buf = malloc(BATCH_SIZE * sizeof(uint32_t));
-  if (!buf) {
-    perror("malloc");
-    close(fd);
     return 1;
   }
 
@@ -30,17 +22,19 @@ int main(void) {
   size_t total = 0;
 
   while (total < NUM_OPS) {
-    ssize_t n = read(fd, buf, BATCH_SIZE * sizeof(uint32_t));
+    uint32_t value;
+    ssize_t n = read(fd, &value, sizeof(uint32_t));
     if (n <= 0)
       break;
-    total += n / sizeof(uint32_t);
+    total++;
+
+    (void)value;
   }
 
   uint64_t elapsed = get_ns() - start;
 
   print_results(total, elapsed);
 
-  free(buf);
   close(fd);
   return 0;
 }
